5385da55589c87577c4d224445f4f8c2f6fa6611
bipface <bipface@users.noreply.github.com>
2017-08-07 21:31:03 +1000

    Add files via upload

---
+++ rule34-autocomplete-key.user.js
@@ -0,0 +1,27 @@
+﻿// ==UserScript==
+// @name        Rule34.xxx: Autocomplete Key
+// @namespace   6930e44863619d3f19806f68f74dbf62
+// @version     2017-08-07
+// @match       *://*.rule34.xxx/*
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+
+'use strict';
+
+/* -------------------------------------------------------------------------- */
+
+if (typeof Awesomplete === `function`) {
+	/* enable the Tab key for autocompletion */
+	for (let A of Awesomplete.all) {
+		A.input.addEventListener(`keydown`, Ev => {
+			if (Ev.key === `Tab`
+				&& A.opened
+				&& A.selected
+			) {
+				Ev.preventDefault();
+				A.select();};});
+	};
+};
+
+/* -------------------------------------------------------------------------- */
